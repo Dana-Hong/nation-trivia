20f069cc2f7f37dc4dcd28d4336f2135102dacc6
Dana <dana.hong@hotmail.com>
2023-07-06 12:43:43 -0400

    add new remote pattern to nextconfig

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "flagcdn.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "",
+      },
     ],
   },
 };
