12db3de50894dd7aa120c98ea4eb4dab1090a3bb
Dana <dana.hong@hotmail.com>
2023-07-06 15:15:12 -0400

    Update quiz region selection page

--- src/app/quizzes/capitals/page.tsx
+++ src/app/quizzes/capitals/page.tsx
@@ -6,16 +6,15 @@ export default function Page({}) {
   return (
     <section className="rg-green-200 flex grow flex-col items-center gap-3 py-2">
       <h1 className="pt-12 text-center text-3xl font-medium underline underline-offset-4 sm:text-4xl">
-        Capital Cities
+       Capital Cities 
       </h1>
       <div className="rg-green-300 mx-auto grid w-full max-w-xl gap-2 pt-8 sm:grid-cols-2 sm:gap-6">
         {REGIONS.map((region) => (
           <div
             key={region}
-            className="rounded-md border py-3 cursor-pointer transition-colors hover:bg-zinc-900 sm:py-10"
+            className="flex cursor-pointer transition-colors hover:bg-zinc-900"
           >
-            <Link
-              className="mx-auto inline-block h-full w-full text-center capitalize sm:text-2xl sm:font-semibold"
+            <Link className="mx-auto h-full w-full text-center capitalize sm:text-2xl sm:font-semibold py-3 sm:py-10 border rounded-md"
               href={`/quizzes/capitals/${region.toLocaleLowerCase()}`}
             >
               {region.toLocaleLowerCase()}
@@ -24,10 +23,10 @@ export default function Page({}) {
         ))}
         <div
           key={"all"}
-          className="rounded-md border py-3 cursor-pointer transition-colors hover:bg-zinc-900 sm:py-10"
+          className="flex cursor-pointer transition-colors hover:bg-zinc-900"
         >
           <Link
-            className="mx-auto inline-block h-full w-full text-center capitalize sm:text-2xl sm:font-semibold"
+            className="mx-auto h-full w-full text-center capitalize sm:text-2xl sm:font-semibold py-3 sm:py-10 border rounded-md"
             href="/quizzes/capitals/all"
           >
             All
--- src/app/quizzes/flags/page.tsx
+++ src/app/quizzes/flags/page.tsx
@@ -5,16 +5,33 @@ import { REGIONS } from "@/app/constants/geography";
 export default function Page({}) {
   return (
     <section className="flex grow flex-col items-center gap-3 py-2">
-      <h1 className="pt-32 text-center text-3xl font-medium sm:text-4xl">Guess the flag</h1>
-      <div className="mx-auto grid w-[70vw] max-w-lg gap-2 pt-8 sm:grid-cols-2">
+      <h1 className="pt-12 text-center text-3xl font-medium underline underline-offset-4 sm:text-4xl">
+        World Flags
+      </h1>
+      <div className="rg-green-300 mx-auto grid w-full max-w-xl gap-2 pt-8 sm:grid-cols-2 sm:gap-6">
         {REGIONS.map((region) => (
-          <Link key={region} href={`/quizzes/flags/${region.toLocaleLowerCase()}`}>
-            <Button className="w-full capitalize">{region.toLocaleLowerCase()}</Button>
-          </Link>
+          <div
+            key={region}
+            className="flex cursor-pointer transition-colors hover:bg-zinc-900"
+          >
+            <Link className="mx-auto h-full w-full text-center capitalize sm:text-2xl sm:font-semibold py-3 sm:py-10 border rounded-md"
+              href={`/quizzes/flags/${region.toLocaleLowerCase()}`}
+            >
+              {region.toLocaleLowerCase()}
+            </Link>
+          </div>
         ))}
-        <Link key={"all"} href={"/quizzes/flags/all"}>
-          <Button className="w-full capitalize">All</Button>
-        </Link>
+        <div
+          key={"all"}
+          className="flex cursor-pointer transition-colors hover:bg-zinc-900"
+        >
+          <Link
+            className="mx-auto h-full w-full text-center capitalize sm:text-2xl sm:font-semibold py-3 sm:py-10 border rounded-md"
+            href="/quizzes/flags/all"
+          >
+            All
+          </Link>
+        </div>
       </div>
       <Link href={"/quizzes"} className="mt-12">
         <Button variant={"secondary"}>Back to Quizzes</Button>
