2e49a2fdbc180f71af2e5f0413e29e623a32682f
Dana <dana.hong@hotmail.com>
2023-07-06 12:42:51 -0400

    Update header and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,19 @@ import GithubIcon from "./icons/github";
 import LinkedInIcon from "./icons/linkedin";
 
 export default function Footer() {
-    return (
-      <footer className="flex flex-col gap-2 items-center justify-center py-4">
-        <p className="text-sm">Built by <a className="cursor-pointer font-bold" href="https://github.com/Dana-Hong" target="_blank">Dana Hong</a> - 2023</p>
-        <div className="flex gap-5">
-          <GithubIcon className="w-5 h-5" />
-          <LinkedInIcon className="w-5 h-5" />
-        </div>
-      </footer>
-    )
+  return (
+    <footer className="flex flex-col items-center justify-center gap-2 border-t py-4 md:py-6">
+      <p className="text-sm md:text-base">
+        Built by{" "}
+        <a className="cursor-pointer font-bold" href="https://github.com/Dana-Hong" target="_blank">
+          Dana Hong
+        </a>{" "}
+        - 2023
+      </p>
+      <div className="flex gap-5">
+        <GithubIcon className="h-5 w-5 md:h-6 md:w-6" />
+        <LinkedInIcon className="h-5 w-5 md:h-6 md:w-6" />
+      </div>
+    </footer>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,40 +17,41 @@ export default function Header() {
 
   return (
     <header className="relative z-50 flex items-center justify-between border-b px-2 py-1">
-      <Link href={"/"} className="inline-flex gap-2">
-        <EarthLogo className="h-6 w-6" />
-        <span className="font-bold">Nation Trivia</span>
+      <Link href={"/"}>
+        <Button variant={"link"} className="gap-2">
+          <EarthLogo className="h-6 w-6" />
+          <span className="text-base font-bold">Nation Trivia</span>
+        </Button>
       </Link>
       <button className="sm:hidden" onClick={handleClick}>
         <BurgerMenu className="h-6 w-6" />
       </button>
       <nav className="hidden gap-4 sm:flex">
-        <Button variant="link">
-          <Link href={"/about"}>About</Link>
-        </Button>
-        <Button variant="link">
-          <Link href={"/countries"}>Countries Database</Link>
-        </Button>
-        <Button variant="link">
-          <Link href={"/quizzes"}>Quizzes</Link>
-        </Button>
-        <Button variant="link">
-          <Link href={"/scores"}>Scores</Link>
-        </Button>
+        <Link href={"/about"}>
+          <Button variant="link">About</Button>
+        </Link>
+        <Link href={"/countries"}>
+          <Button variant="link">Countries Database</Button>
+        </Link>
+        <Link href={"/quizzes"}>
+          <Button variant="link">Quizzes</Button>
+        </Link>
+        <Link href={"/scores"}>
+          <Button variant="link">Scores</Button>
+        </Link>
       </nav>
       <nav
         className={`${
           !open ? "-translate-x-full" : "-translate-x-0"
         } absolute left-0 top-0 flex h-screen w-full transition-transform`}
       >
-        <div className="flex h-full w-3/4 max-w-lg flex-col gap-2 bg-zinc-900 px-8 pt-8">
+        <div className="flex h-full w-3/4 max-w-lg flex-col gap-2 bg-zinc-950 px-8 pt-8">
           <div className="flex justify-between border-b">
-            <Link
-              href={"/"}
-              className="py-2 text-xl font-medium text-zinc-200"
-              onClick={handleClick}
-            >
-              Home
+            <Link href={"/"} className="flex gap-2 py-2">
+              <EarthLogo className="h-6 w-6 fill-zinc-200" />
+              <span className="block pb-2 text-base font-semibold text-zinc-200">
+                Nation Trivia
+              </span>
             </Link>
             <div onClick={handleClick}>
               <Close className="-mr-2 -mt-3 h-7 w-7 fill-zinc-200" />
@@ -75,7 +76,7 @@ export default function Header() {
         </div>
         <div
           className={`grow ${
-            !open ? "bg-transparent" : "bg-zinc-800 bg-opacity-90"
+            !open ? "bg-transparent" : "bg-zinc-900 bg-opacity-90"
           } transition-colors delay-100`}
           onClick={handleClick}
         ></div>
