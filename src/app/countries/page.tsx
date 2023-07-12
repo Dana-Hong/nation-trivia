3c195f57de094d5a98fd18aab152af873a70f23d
Dana <dana.hong@hotmail.com>
2023-07-12 18:11:24 -0400

    Update table styling

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -3,11 +3,13 @@ import { columns } from "./columns";
 import { getAllCountries, sortCountriesAlphabetic } from "../utils";
 
 export default async function Page() {
-  const data = await getAllCountries().then(countries => sortCountriesAlphabetic(countries, true));
+  const data = await getAllCountries().then((countries) =>
+    sortCountriesAlphabetic(countries, true)
+  );
 
   return (
-    <section className="grow py-20">
-      <h1 className="text-lg sm:text-2xl md:text-3xl font-medium max-w-screen-2xl mx-auto pb-8">Countries</h1>
+    <section className="mx-auto max-w-screen-2xl grow py-20 pb-8">
+      <h1 className="pb-8 text-lg font-medium sm:text-2xl md:text-3xl">Countries</h1>
       <DataTable columns={columns} data={data} />
     </section>
   );
