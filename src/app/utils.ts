fa0ab81076f68f7bead2a85bc6005a7af6239834
Dana <dana.hong@hotmail.com>
2023-08-25 14:55:02 -0400

    Add sort and filter util functions

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,6 +1,7 @@
 import { Region } from "./types";
 import { Country } from "./types";
 import { REST_COUNTRIES_URL } from "./constants/geography";
+import { Continents } from "./countries/countries-list";
 
 export async function getAllCountries() {
   const response = await fetch(`${REST_COUNTRIES_URL}/all`);
@@ -25,9 +26,7 @@ export async function getCountryByCode(code: string) {
 
 export async function getCountryByName(name: string) {
   const countryName = name.split(" ").join("%20");
-  const response = await fetch(
-    `${REST_COUNTRIES_URL}/name/${countryName}/?fullText=true`
-  );
+  const response = await fetch(`${REST_COUNTRIES_URL}/name/${countryName}/?fullText=true`);
   if (!response.ok) {
     throw new Error(`Failed to fetch ${name}`);
   }
@@ -39,7 +38,9 @@ export async function getCountryByName(name: string) {
 
 export async function getCountriesByRegion(region: Region, noCache?: boolean) {
   const formattedRegion = region.toLocaleLowerCase();
-  const response = noCache ? await fetch(`${REST_COUNTRIES_URL}/region/${formattedRegion}`, { next: { revalidate: 0 }}) : await fetch(`${REST_COUNTRIES_URL}/region/${formattedRegion}`);
+  const response = noCache
+    ? await fetch(`${REST_COUNTRIES_URL}/region/${formattedRegion}`, { next: { revalidate: 0 } })
+    : await fetch(`${REST_COUNTRIES_URL}/region/${formattedRegion}`);
   if (!response.ok) {
     throw new Error(`Failed to fetch countries in ${region}`);
   }
@@ -51,23 +52,39 @@ export async function getCountriesByRegion(region: Region, noCache?: boolean) {
 export async function getCountriesByCapital(capital: string) {
   const response = await fetch(`${REST_COUNTRIES_URL}/capital/${capital}`);
   if (!response.ok) {
-    throw new Error(
-      `Failed to fetch the country with the capital city of ${capital}`
-    );
+    throw new Error(`Failed to fetch the country with the capital city of ${capital}`);
   }
 
   const data = response.json();
   return data;
 }
 
-export function sortCountriesAlphabetic(
-  countriesList: Country[],
-  ascending: boolean
-) {
+export function sortCountries(countriesList: Country[], sortValue: string) {
+  if (sortValue === "nameAsc") {
+    return sortCountriesAlphabetic(countriesList, true);
+  } else if (sortValue === "nameDesc") {
+    return sortCountriesAlphabetic(countriesList, false);
+  }
+}
+
+export function sortCountriesAlphabetic(countriesList: Country[], ascending: boolean) {
   return countriesList.sort((a: Country, b: Country) => {
     if (ascending) return a.name.common.localeCompare(b.name.common);
     return b.name.common.localeCompare(a.name.common);
   });
+}
+
+export function filterCountriesByContinent(countriesList: Country[], continent: Continents) {
+  if (continent.all) return countriesList;
+  const filteredCountries = countriesList.filter((country) => {
+    for (let i = 0; i < country.continents.length; i++) {
+      if (continent[country.continents[i] as keyof Continents]) return true;
+    }
+
+    return false;
+  });
+
+  return filteredCountries;
 }
 
 export async function getMultiChoiceQuiz(region: Region, difficulty: number) {
